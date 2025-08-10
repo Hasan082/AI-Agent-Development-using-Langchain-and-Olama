bf7c65cebf42757bc249e1a519532f5166d544fb
Md Hasanuzzaman <dr.has82@gmail.com>
2025-08-11 02:38:54 +0600

    memory added

--- LLM/simplechat/simplechat.py
+++ LLM/simplechat/simplechat.py
@@ -1,19 +1,43 @@
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.prompts import PromptTemplate
 from langchain_ollama import OllamaLLM
 
-llm = OllamaLLM(model='mistral')
+# Load the AI Model
+llm = OllamaLLM(model="mistral")
+
+# Initiate Hiostory
+chat_history = ChatMessageHistory()
+
+prompt = PromptTemplate(
+    input_variables=["chat_history", "question"],
+    template="Prevoius Conversion: {chat_history}\nUser:m {question}\n AI",
+)
 
 
-print("Welcome to Our AI Agent! Ask me anything about our services!")
+def run_chain(question):
+    chat_history_text = "\n".join(
+        f"{msg.type.capitalize()}: {msg.content}" for msg in chat_history.messages
+    )
+    response = llm.invoke(
+        prompt.format(chat_history=chat_history_text, question=question)
+    )
 
-while True:
-    question = input("Your question or type \'exit\' to stop chating: ")
-    
-    if question.lower().strip() == 'exit':
-        print("GoodBye")
-        break
-    
-    answer = llm.invoke(question)
-    
-    print(f"AI Agent: {answer}")
-    print()
-    
+    chat_history.add_user_message(question)
+    chat_history.add_ai_message(response)
+
+    return response
+
+
+print("Chatboot with memory! Ask me anything about our services!")
+
+# while True:
+#     question = input("Your question or type 'exit' to stop chating: ")
+
+#     if question.lower().strip() == "exit":
+#         print("GoodBye")
+#         break
+
+#     answer = llm.invoke(question)
+
+#     print(f"AI Agent: {answer}")
+#     print()
