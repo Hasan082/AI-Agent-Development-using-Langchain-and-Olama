10859c912360bda5409845cb69c528b930c2d8ec
Md Hasanuzzaman <dr.has82@gmail.com>
2025-08-11 01:59:04 +0600

    Simple ai agent done

--- LLM/simplechat/simplechat.py
+++ LLM/simplechat/simplechat.py
@@ -1,2 +1,19 @@
-from langchaun_ollama import Ollmaman
+from langchain_ollama import OllamaLLM
 
+llm = OllamaLLM(model='mistral')
+
+
+print("Welcome to Our AI Agent! Ask me anything about our services!")
+
+while True:
+    question = input("Your question or type \'exit\' to stop chating: ")
+    
+    if question.lower().strip() == 'exit':
+        print("GoodBye")
+        break
+    
+    answer = llm.invoke(question)
+    
+    print(f"AI Agent: {answer}")
+    print()
+    
