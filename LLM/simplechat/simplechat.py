dda10a95e32b1a305c16fbde063a7ab41bdf5fe2
Md Hasanuzzaman <dr.has82@gmail.com>
2025-08-12 01:07:47 +0600

    Upadted code

--- LLM/simplechat/simplechat.py
+++ LLM/simplechat/simplechat.py
@@ -2,33 +2,58 @@ from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.prompts import PromptTemplate
 from langchain_ollama import OllamaLLM
 
-# Load the AI Model
+# Load AI 
 llm = OllamaLLM(model="mistral")
 
-# Initiate Hiostory
+# Initialize Memory
 chat_history = ChatMessageHistory()
 
-prompt = PromptTemplate(
-    input_variables=["chat_history", "question"],
-    template="Prevoius Conversion: {chat_history}\nUser:m {question}\n AI",
+# AI Chat Pronpt
+promots = PromptTemplate(
+    input_variables=['chat_history', 'question'],
+    template="Prevous Conversion: {chat_history}\n User: {question} AI"    
 )
 
-
 def run_chain(question):
-    chat_history_text = "\n".join(
-        f"{msg.type.capitalize()}: {msg.content}" for msg in chat_history.messages
-    )
-    response = llm.invoke(
-        prompt.format(chat_history=chat_history_text, question=question)
-    )
-
-    chat_history.add_user_message(question)
-    chat_history.add_ai_message(response)
-
-    return response
+    # Load chat history text
+    chat_history_text = "/n".join([f"{msg.type.capitalize()}" for msg in chat_history.messages])
 
 
-print("Chatboot with memory! Ask me anything about our services!")
+
+
+
+
+# from langchain_community.chat_message_histories import ChatMessageHistory
+# from langchain_core.prompts import PromptTemplate
+# from langchain_ollama import OllamaLLM
+
+# # Load the AI Model
+# llm = OllamaLLM(model="mistral")
+
+# # Initiate Hiostory
+# chat_history = ChatMessageHistory()
+
+# prompt = PromptTemplate(
+#     input_variables=["chat_history", "question"],
+#     template="Prevoius Conversion: {chat_history}\nUser:m {question}\n AI",
+# )
+
+
+# def run_chain(question):
+#     chat_history_text = "\n".join(
+#         f"{msg.type.capitalize()}: {msg.content}" for msg in chat_history.messages
+#     )
+#     response = llm.invoke(
+#         prompt.format(chat_history=chat_history_text, question=question)
+#     )
+
+#     chat_history.add_user_message(question)
+#     chat_history.add_ai_message(response)
+
+#     return response
+
+
+# print("Chatboot with memory! Ask me anything about our services!")
 
 # while True:
 #     question = input("Your question or type 'exit' to stop chating: ")
