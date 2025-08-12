346f4a582ecf4e9c4fdfa07d405d47b606561643
Md Hasanuzzaman <dr.has82@gmail.com>
2025-08-12 21:01:35 +0600

    Ready with memory

--- LLM/simplechat/simplechat.py
+++ LLM/simplechat/simplechat.py
@@ -17,31 +17,35 @@ prompt = PromptTemplate(
 
 def run_chain(question):
     # Load chat history text
-    chat_history_text = "/n".join(
+    chat_history_text = "\n".join(
         [f"{msg.type.capitalize()}: {msg.content}" for msg in chat_history.messages]
     )
-    response = llm.invoke(prompt.format(chat_history=chat_history_text, question=question))
-    
+    response = llm.invoke(
+        prompt.format(chat_history=chat_history_text, question=question)
+    )
+
     chat_history.add_user_message(question)
     chat_history.add_ai_message(response)
 
     return response
 
+
 print("Your AI Assistant with Memory")
 print("Chatboot with memory! Ask me anything about our services!")
-print("Type \'exit\' for stop chat")
+print("Type 'exit' for stop chat")
 
 
 while True:
     user_input = input("You: ")
-    
-    if user_input.lower().strip() == 'exit':
+
+    if user_input.lower().strip() == "exit":
         print("Goodbye Today")
         break
-    
+
     ai_response = run_chain(user_input)
-    
+
     print(f"AI Bot: {ai_response}")
     print()
 
 
+# ToDo: https://gale.udemy.com/course/mastering-ai-agents-bootcamp-build-smart-chatbots-tools/learn/lecture/48844581#overview
